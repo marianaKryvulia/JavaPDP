14974f85aee739def91a2182aa019c00d5840ddf
Mariana_Kryvulia <mkryvulya@gmail.com>
2018-04-19 13:33:15 +0300

    removed all static specifiers. Now I don't know how to call non-static methods from main method

--- src/main/java/com/epam/java/pdp/figures/Shape.java
+++ src/main/java/com/epam/java/pdp/figures/Shape.java
@@ -2,49 +2,54 @@ package com.epam.java.pdp.figures;
 import com.epam.java.pdp.figures.Point;
 
 public class Shape {
-    static int x1 = 1;
-    static int x2 = 2;
-    static int x3 = 3;
-    static int y1 = 2;
-    static int y2 = 1;
-    static int y3 = 3;
-    static Point p1 = new Point(x1, y1);
-    static Point p2 = new Point(x2, y2);
-    static Point p3 = new Point(x3, y3);
-    static Line ln1 = new Line(p1,p2);
-    static Line ln2 = new Line(p2,p3);
-    static Line ln3 = new Line(p3,p1);
-    static double a = ln1.lineLength();
-    static double b = ln2.lineLength();
-    static double c = ln3.lineLength();
+    int x1 = 1;
+    int x2 = 2;
+    int x3 = 3;
+    int y1 = 2;
+    int y2 = 1;
+    int y3 = 3;
+    Point p1 = new Point(x1, y1);
+    Point p2 = new Point(x2, y2);
+    Point p3 = new Point(x3, y3);
+    Line ln1 = new Line(p1,p2);
+    Line ln2 = new Line(p2,p3);
+    Line ln3 = new Line(p3,p1);
+    double a = ln1.lineLength();
+    double b = ln2.lineLength();
+    double c = ln3.lineLength();
 
    public static void main (String[] args){
-       System.out.println("Line lengths are: ");
-       System.out.println(a);
-       System.out.println(b);
-       System.out.println(c);
-
+       printLineLength();
        isTriangle();
        calcPerimeter();
        isTriangleRectangular();
        isTriangleIsosceles();
     }
+   double printLineLength(){
+        System.out.println("Line lengths are: ");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        return a;
+    }
 
-  static  double calcPerimeter(){
+   double calcPerimeter(){
         double perimeter = ln1.lineLength() + ln2.lineLength() + ln3.lineLength();
         System.out.println("Perimeter is: " + perimeter);
         return perimeter;
     }
-  static boolean isTriangle(){
+   boolean isTriangle(){
        if ((ln1.start == ln2.start | ln1.start == ln2.end | ln1.start == ln3.start | ln1.start == ln3.end ) &&
-               (ln1.end == ln2.start | ln1.end == ln2.end | ln1.end == ln3.start | ln1.end == ln3.start)
+               (ln1.end == ln2.start | ln1.end == ln2.end | ln1.end == ln3.start | ln1.end == ln3.start) &&
+               (ln2.start == ln3.start | ln2.start == ln3.end | ln2.start == ln1.start | ln2.start == ln1.end ) &&
+               (ln2.end == ln3.start | ln2.end == ln3.end | ln2.end == ln1.start | ln2.end == ln1.start)
                )
        {System.out.println("This is a triangle. ");
        return true;}
        else return false;
     }
 
-    static boolean isTriangleRectangular(){
+    boolean isTriangleRectangular(){
        if (((a*a) == ((b*b) + (c*c))) ||
             ((b*b) == ((a*a) + (c*c))) ||
             ((c*c) == ((a*a) + (b*b))))
@@ -54,7 +59,7 @@ public class Shape {
             {System.out.println("Triangle is not Rectangular.");
             return false;}
 }
-  static boolean isTriangleIsosceles (){
+   boolean isTriangleIsosceles (){
        if (a == b || b == c || c ==a )
        {System.out.println("Triangle is Isosceles.");
            return true;}
--- src/main/java/com/epam/java/pdp/oca/TShirtApp.java
+++ src/main/java/com/epam/java/pdp/oca/TShirtApp.java
@@ -4,13 +4,12 @@ import com.epam.java.pdp.Second;
 
 public class TShirtApp {
     public static void main (String[] args){
-       TShirt defaultTshirt = new TShirt();
+        TShirt defaultTshirt = new TShirt();
        System.out.println(defaultTshirt);
        TShirt tshirtWithModifiedRequiredAttributes = new TShirt("female","m","pink");
        System.out.println(tshirtWithModifiedRequiredAttributes);
        TShirt tshirtWithOptionalAttributes = new TShirt("kid","xs","green","kidsStyle","yellow");
        System.out.println(tshirtWithOptionalAttributes);
-
         String sex = Second.generateString(3);
         String size = Second.generateString(2);
         String color = Second.generateString(10);
